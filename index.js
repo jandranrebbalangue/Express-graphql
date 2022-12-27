0ece0985abc1af5327408d4b2cf56760201b77a8
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2022-12-27 21:44:44 +0800

    Refactor and add editorconfig

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = express();
 
 const fs = require("fs");
 
-const schemaDates = buildSchema(`
+const schema = buildSchema(`
   type NumDays {
     num_days: Int
   }
-
-  type Query {
-    dates(start_date:String,end_date:String):NumDays
-  }
-`);
-
-const schemaContents = buildSchema(`
- type File {
-    file_size:Int
-  }
-
-  type Query {
-    getContents(contents:String): File
+  type File {
+  file_size:Int
   }
 
   type Mutation {
-    saveContents(contents:String):File
+    saveContents(contents:String):File 
+  }
+
+  type Query {
+    dates(start_date:String,end_date:String):NumDays,
+    getContents(contents:String):File
   }
 `);
 
@@ -35,30 +29,21 @@ class File {
   }
 
   file_size() {
-    fs.writeFile("./contents.txt", this.contents, function(err) {
+    fs.writeFile("./contents.txt", this.contents, function (err) {
       if (err) {
         return console.log(err);
       }
     });
-    const stats = fs.statSync("contents.txt")
+    const stats = fs.statSync("contents.txt");
     const fileSizeInBytes = stats.size;
-    return fileSizeInBytes
+    return fileSizeInBytes;
   }
 }
-
-const rootValueContents = {
-  getContents: ({ contents }) => {
-    return new File(contents)
-  },
-  saveContents: ({ contents }) => {
-    return new File(contents)
-  },
-};
 
 class NumDays {
   constructor(startDate, endDate) {
     this.start_date = startDate;
-    this.end_date = endDate
+    this.end_date = endDate;
   }
 
   num_days() {
@@ -67,33 +52,30 @@ class NumDays {
     const differenceInTime = endDate.getTime() - startDate.getTime();
 
     const betweenDays = differenceInTime / (1000 * 3600 * 24);
-    return betweenDays
+    return betweenDays;
   }
-
 }
-const rootValueDates = {
+
+const rootValue = {
   dates: ({ start_date, end_date }) => {
-    return new NumDays(start_date, end_date)
+    return new NumDays(start_date, end_date);
+  },
+  saveContents: ({ contents }) => {
+    return new File(contents);
+  },
+  getContents: ({ contents }) => {
+    return new File(contents);
   },
 };
 
 // Construct a schema, using GraphQL schema language
 app.use(
-  '/dates',
+  "/exam",
   graphqlHTTP({
-    schema: schemaDates,
-    rootValue: rootValueDates,
+    schema,
+    rootValue: rootValue,
     graphiql: true,
-  }),
-);
-
-app.use(
-  '/contents',
-  graphqlHTTP({
-    schema: schemaContents,
-    rootValue: rootValueContents,
-    graphiql: true,
-  }),
+  })
 );
 
 app.listen(4000);
